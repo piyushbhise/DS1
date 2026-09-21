b3a670d5ac5bbdb7090002523120dfcfc652ebd2
piyushbhise <piyushbhise309@gmail.com>
2026-09-21 14:27:12 +0530

    Add files via upload

---
+++ student__.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+class student
+{
+    public:
+    int rollno;
+    void input()
+{
+    cout<<"ROLL NO OF THE STUDENT IS :"<<endl;
+    cin>>rollno;
+}
+    void display()
+{
+    cout<<"ROLL NO ->:"<<rollno<<endl;
+}
+};
+int main()
+{
+    student s1,s2,s3,s4,s5;
+      s1.input();
+      s1.display();
+
+      s2.input();
+      s2.display();
+
+      s3.input();
+      s3.display();
+
+      s4.input();
+      s4.display();
+
+      s5.input();
+      s5.display();
+
+      return 0;
+}
